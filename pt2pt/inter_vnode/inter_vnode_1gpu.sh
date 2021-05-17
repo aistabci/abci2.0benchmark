041bedc43b788cd6126a54bbdd9014b7d3a7b00d
Shinichiro Takizawa <s.takizawa@gmail.com>
2021-05-17 20:35:05 +0900

    Add a new bencharmk result

---
+++ pt2pt/inter_vnode/inter_vnode_1gpu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#$-l rt_F=2
+#$-cwd
+#$-l h_rt=0:10:00
+
+source /etc/profile.d/modules.sh
+module load cuda/11.2/11.2.2
+module load gcc/7.4.0
+module load openmpi/4.0.5
+module load gdrcopy/2.0
+
+export UCX_HOME=/apps/centos7/ucx/1.7.0/gcc7.4.0_cuda11.2.2_gdrcopy2.0
+export LD_LIBRARY_PATH=${UCX_HOME}/lib:${LD_LIBRARY_PATH}
+
+WORKDIR=`pwd`
+OMBDIR=${WORKDIR}/../omb-5.7-vnode/libexec/osu-micro-benchmarks
+PROG_LATENCY=${OMBDIR}/mpi/pt2pt/osu_latency
+PROG_BANDWIDTH=${OMBDIR}/mpi/pt2pt/osu_bw
+#PROG_OPTION="-m 8388608 -d cuda"  # error occurs in bandwidth test
+PROG_OPTION="-m 4194304 -d cuda"
+
+# number of processes/node (GPUs)
+NPPN=1
+# total MPI processes
+NMPIPROCS=$(($NHOSTS * $NPPN))
+
+mpirun -np $NMPIPROCS --map-by ppr:${NPPN}:node -tag-output hostname
+
+mpirun -np $NMPIPROCS --map-by ppr:${NPPN}:node \
+       --mca btl_openib_want_cuda_gdr 1 \
+       -x PATH \
+       -x LD_LIBRARY_PATH \
+       ${PROG_LATENCY} ${PROG_OPTION} D D > ${JOB_NAME}.latency
+
+mpirun -np $NMPIPROCS --map-by ppr:${NPPN}:node \
+       --mca btl_openib_want_cuda_gdr 1 \
+       -x PATH \
+       -x LD_LIBRARY_PATH \
+       ${PROG_BANDWIDTH} ${PROG_OPTION} D D > ${JOB_NAME}.bandwidth
+
