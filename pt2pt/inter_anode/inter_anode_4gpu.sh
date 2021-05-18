1ba479dc0b0ab870a2ad16a69fac720161ed8cf0
Shinichiro Takizawa <s.takizawa@gmail.com>
2021-05-18 11:38:07 +0900

    Add new benchmark scripts and results

---
+++ pt2pt/inter_anode/inter_anode_4gpu.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#$-l rt_AF=2
+#$-cwd
+#$-l h_rt=0:10:00
+
+source /etc/profile.d/modules.sh
+module load cuda/11.2/11.2.2
+module load openmpi/4.0.5
+module load gdrcopy/2.1
+
+export UCX_HOME=/apps/rhel8/ucx/1.9.0/gcc8.3.1_cuda11.2.2_gdrcopy2.1
+export LD_LIBRARY_PATH=${UCX_HOME}/lib:${LD_LIBRARY_PATH}
+
+WORKDIR=`pwd`
+OMBDIR=${WORKDIR}/../../programs/omb-5.7-anode/libexec/osu-micro-benchmarks
+PROG_LATENCY=${OMBDIR}/mpi/pt2pt/osu_multi_lat
+PROG_BANDWIDTH=${OMBDIR}/mpi/pt2pt/osu_mbw_mr
+PROG_OPTION="-m 4194304 -d cuda"
+
+# number of processes/node (GPUs)
+NPPN=4
+# total MPI processes
+NMPIPROCS=$(($NHOSTS * $NPPN))
+# number of processes/socket
+NPPS=$(($NPPN / 2))
+
+mpirun -np $NMPIPROCS --map-by ppr:${NPPS}:socket -tag-output hostname
+
+mpirun -np $NMPIPROCS --map-by ppr:${NPPS}:socket \
+       --mca btl_openib_want_cuda_gdr 1 \
+       -x PATH \
+       -x LD_LIBRARY_PATH \
+       -x UCX_WARN_UNUSED_ENV_VARS=n \
+       ${PROG_LATENCY} ${PROG_OPTION} D D > ${JOB_NAME}.latency
+
+mpirun -np $NMPIPROCS --map-by ppr:${NPPS}:socket \
+       --mca btl_openib_want_cuda_gdr 1 \
+       -x PATH \
+       -x LD_LIBRARY_PATH \
+       -x UCX_WARN_UNUSED_ENV_VARS=n \
+       ${PROG_BANDWIDTH} ${PROG_OPTION} D D > ${JOB_NAME}.bandwidth
+
