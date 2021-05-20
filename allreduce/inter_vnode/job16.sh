583acee81a7a3954d2af7dd83d040f9293509da2
Shinichiro Takizawa <s.takizawa@gmail.com>
2021-05-20 18:03:33 +0900

    Add benchmark results

---
+++ allreduce/inter_vnode/job16.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#$-l rt_F=16
+#$-cwd
+#$-l h_rt=0:10:00
+
+source /etc/profile.d/modules.sh
+
+# number of processes/node (GPUs)
+NPPN=4
+# total MPI processes
+NMPIPROCS=$(($NHOSTS * $NPPN))
+# number of processes/socket
+NPPS=$(($NPPN / 2))
+
+module load gcc/7.4.0
+module load openmpi/4.0.5
+mpirun -np $NMPIPROCS --map-by ppr:${NPPS}:socket -tag-output hostname
+module purge
+
+source ./mpi.sh "${JOB_NAME}.${JOB_ID}" $NMPIPROCS $NPPN $NPPS
+source ./mpi-cpu.sh "${JOB_NAME}.${JOB_ID}" $NMPIPROCS $NPPN $NPPS
+source ./nccl.sh "${JOB_NAME}.${JOB_ID}" $NMPIPROCS $NPPN $NPPS
+
