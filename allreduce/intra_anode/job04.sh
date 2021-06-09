7cbfa36b9ed7119652f5e0f741964f4c023f83f4
Shinichiro Takizawa <s.takizawa@gmail.com>
2021-06-09 18:33:19 +0900

    Update benchmark scripts and results

---
+++ allreduce/intra_anode/job04.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#$-l rt_AF=1
+#$-cwd
+#$-l h_rt=0:10:00
+
+source /etc/profile.d/modules.sh
+
+OUTPUT_PREFIX=${JOB_NAME}.${JOB_ID}
+
+# number of processes/node (GPUs)
+NPPN=4
+# total MPI processes
+NMPIPROCS=$(($NHOSTS * $NPPN))
+# number of processes/socket
+NPPS=$(($NPPN / 2))
+
+module load openmpi/4.0.5
+mpirun -np $NMPIPROCS --map-by ppr:${NPPS}:socket -tag-output hostname
+module purge
+
+source ../mpi_anode.sh
+source ../mpi-cpu_anode.sh
+source ../nccl_anode.sh
